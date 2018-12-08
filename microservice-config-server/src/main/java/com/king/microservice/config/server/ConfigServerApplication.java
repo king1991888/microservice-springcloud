85b9efa7d23008c358727827e168a2482955f47d
king <278362446@qq.com>
2018-12-09 00:19:48 +0800

    修改springcloud版本为finchely.SR1,springboot版本为2.0.4.并加入配置中心

--- microservice-config-server/src/main/java/com/king/microservice/config/server/ConfigServerApplication.java
+++ microservice-config-server/src/main/java/com/king/microservice/config/server/ConfigServerApplication.java
@@ -3,6 +3,7 @@ package com.king.microservice.config.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author huangjie
@@ -11,6 +12,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  */
 @SpringBootApplication
 @EnableConfigServer   //启用配置中心服务端
+@EnableEurekaClient
 public class ConfigServerApplication {
 
 
---
+++ microservice-discovery-eureka-server/src/main/java/com/king/microservice/eureka/server/config/WebSecurityConfig.java
@@ -0,0 +1,19 @@
+package com.king.microservice.eureka.server.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author king
+ * 2018/12/8
+ */
+@EnableWebSecurity   //关闭 crsf，允许服务注册到eureka
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        super.configure(http);
+    }
+}
